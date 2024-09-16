3144f618c70ad995c24b3b5fe575ead6bdf944ad
Choudhry Abdullah <cabdulla@trinity.edu>
2024-09-16 11:45:06 -0500

    changed the repl to stop inputs when quit is given

--- Main.hs
+++ Main.hs
@@ -3,45 +3,47 @@ import Lexer
 import Eval
 
 repl :: IO ()
-repl = loop Context{env = [], store = [], classes =[]} 1
-    where
-        loop context count = do
-            (c,input) <- getBlock
-            do
-                case scanTokens input of
-                    Just [] -> loop context (count + c)
-                    Just tokens -> case parse tokens of
-                        Just statement -> case evalD context statement of
-                            Just (UnitVal, newContext) -> do
-                                loop newContext (count + c)
-                            Just (val, newContext) -> do
-                                putStr "> "
-                                putStrLn $ show val
-                                loop newContext (count + c)
-                            Nothing -> putStrLn $ "Knobhead made an error on " ++ show (count+c)
-                        Nothing -> putStrLn $ "Bonkers on line " ++ show (count+c)
-                    Nothing -> putStrLn $ "Loony on line " ++ show (count+c)
+repl = loop Context {env = [], store = [], classes = []} 1
+  where
+    loop context count = do
+      (c, tokens) <- getBlock
+      if null tokens then
+        loop context (count + c)  -- No tokens, continue REPL loop
+      else
+        case parse tokens of
+          Just QuitS -> putStr "> Thank you for using the interpreter"
+          Just statement ->
+            case evalD context statement of
+              Just (UnitVal, newContext) -> loop newContext (count + c)
+              Just (val, newContext) -> do
+                putStr "> "
+                print val
+                loop newContext (count + c)
+              Nothing -> do
+                putStrLn $ "Evaluation error on line " ++ show (count + c)
                 loop context (count + c)
-
+          Nothing -> do
+            putStrLn $ "Parsing error on line " ++ show (count + c)
+            loop context (count + c)
 
 main :: IO ()
 main = repl
 
 getBlock :: IO (Int, [Token])
 getBlock = aux 0 []
-  where 
+  where
     aux :: Int -> [Token] -> IO (Int, [Token])
-    aux lineCount prevStr = do
+    aux lineCount prevTokens = do
       line <- getLine
-{-       let newLine = concat [prevStr, "\n", line] -}
       case scanTokens line of
-        Just [] -> aux (lineCount+1) prevStr  -- Continue if line is empty
-        Just tks ->
-            return (lineCount + 1, endCheck tks [])  -- Return the accumulated string and line count
-        Nothing -> return (lineCount + 1, [])          -- Continue reading input if scanning fails and increment line count
+        Just [] -> aux (lineCount + 1) prevTokens  -- Continue if line is empty
+        Just tks -> return (lineCount + 1, endCheck tks prevTokens)  -- Check tokens
+        Nothing -> do
+          putStrLn $ "Scanning failed on line " ++ show (lineCount + 1)
+          return (lineCount, prevTokens)
 
 endCheck :: [Token] -> [Token] -> [Token]
 endCheck [] out = out
-endCheck (x:xs) out = case x of 
-    QuitTok -> out ++ [QuitTok]
-    others -> endCheck xs (out ++ [x])
+endCheck (x:xs) out = case x of
+  QuitTok -> out ++ [QuitTok]  -- Stop if QuitTok is encountered
+  _       -> endCheck xs (out ++ [x])
